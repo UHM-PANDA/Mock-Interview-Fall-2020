dadb48d31870cb9abd8645b4e05631342c0ebaf0
kanaigooding <65993942+kanaigooding@users.noreply.github.com>
2020-11-09 20:20:59 -1000

    Java Collatz

--- Week_09/03_Test_Collatz/Java/main.java
+++ Week_09/03_Test_Collatz/Java/main.java
@@ -25,17 +25,18 @@ public class Main {
           }
      }
 
-     public boolean testCollatzInduction(long n){
-          // get some "testedTo" that you know every number under n will converge to 1
-          // "testedTo" will be large in this case I will assume tested to is 2^(32) - 1 (4294967295)
-          long testedTo = 4294967295;
-          if(n <= testedTo)
+     public static boolean testCollatzInduction(long n){
+        // get some "testedTo" that you know every number under n will converge to 1
+        // "testedTo" will be large in this case I will assume tested to is 2^(32) - 1 (4294967295)
+	       long testedTo = 4294967295L;
+
+            if(n <= testedTo)
                return true;
 
           while(true) {
                if (n % 2 == 0) {
                     n = n >> 2;
-                    if (n == 1 || n <= testedTo)
+                    if (n == 1 )
                          return true;
                } else {
                     n = ((n<<1) + n) + 1;
