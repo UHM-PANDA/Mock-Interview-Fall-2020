54bb0f953ade9a67c044875033cbb38a7fc45519
kanaigooding <65993942+kanaigooding@users.noreply.github.com>
2020-11-09 22:37:02 -1000

    Pascals Triangle Java

--- Week_09/02_Pascals_Triangle/Java/main.java
+++ Week_09/02_Pascals_Triangle/Java/main.java
@@ -1,13 +1,27 @@
 public class Main {
-     static void printPascal(int n) {
+     static int[][] printPascal(int n) {
 
-          triangle = new int[10][10];
+          int[][] triangle = new int[n][n];
+          int triangle[0][0] = 1;
 
+          for (int line = 1; line < n; line++) {
+               triangle[line][0] = 1;
+               triangle[line][line] = 1;
+               for (int i = 1; i < line; i++) {
+                    triangle[line][i] = triangle[line - 1][i] + triangle[line - 1][i - 1];
+               }
+          }
+          return triangle;
+     }
+
+     static int[][] printPascalchoose(int n) {
+          int[][] triangle = new int[n][n];
           for (int line = 0; line < n; line++) {
                for (int i = 0; i <= line; i++) {
                     triangle[line][i] = choose(line, i);
                }
           }
+          return triangle;
      }
 
      // some bs for choose function
