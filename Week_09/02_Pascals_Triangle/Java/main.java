edcf984adfdffca5a45e0864c9855c4f891f8f35
kanaigooding <65993942+kanaigooding@users.noreply.github.com>
2020-11-09 20:33:55 -1000

    Pascals Triangle - Java

--- Week_09/02_Pascals_Triangle/Java/main.java
+++ Week_09/02_Pascals_Triangle/Java/main.java
@@ -1,3 +1,24 @@
 public class Main {
+     static void printPascal(int n) {
+          for (int line = 0; line < n; line++) {
+               for (int i = 0; i <= line; i++) {
+                    System.out.print(choose(line, i)+" ");
+               }
+               System.out.println();
+          }
+     }
+
+     // some bs for choose function
+     static int choose(int n, int k) {
+          int res = 1;
+          if (k > n - k) {
+               k = n - k;
+          }
+          for (int i = 0; i < k; ++i) {
+               res *= (n - i);
+               res /= (i + 1);
+          }
+          return res;
+     }
 
 }
