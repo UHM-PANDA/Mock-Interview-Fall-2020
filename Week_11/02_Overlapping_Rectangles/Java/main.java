701b739dd2136111de36bbd00c0eb3439da5a75c
kanaigooding <65993942+kanaigooding@users.noreply.github.com>
2020-11-30 20:25:40 -1000

    Week11.md

--- Week_11/02_Overlapping_Rectangles/Java/main.java
+++ Week_11/02_Overlapping_Rectangles/Java/main.java
@@ -1,3 +1,37 @@
 public class Main {
+     public static boolean overlappingRectangles2D(double[][] rec1, double[][] rec2) {
 
+          // Q.O.L.
+          int X = 0;
+          int Y = 1;
+
+          // find diagonal corners
+          double[] topLeft1 = topLeftCorner(rec1);
+          double[] bottomRight1 = bottomRightCorner(rec1);
+          double[] topLeft2 = topLeftCorner(rec2);
+          double[] bottomRight2 = bottomRightCorner(rec2);
+
+          // check if rec1 is right of rec2
+          if ( topLeft1[X] >= bottomRight2[X] ) {
+               return false;
+          }
+
+          // check if rec2 is right of rec1
+          if ( topLeft2[X] >= bottomRight1[X] ) {
+               return false;
+          }
+
+          // check if rec1 is under rec2
+          if ( topLeft1[Y] <= bottomRight2[Y] ) {
+               return false;
+          }
+
+          // check if rec2 is under rec1
+          if ( topLeft2[Y] <= bottomRight1[Y] ) {
+               return false;
+          }
+
+          // else
+          return true;
+     }
 }
--- Week_11/week11.md
+++ Week_11/week11.md
@@ -7,10 +7,17 @@ Input: 	6
 Output:	[2, 3, 5]
 ```
 #### 2) Overlapping Rectangles
+Given the vertices of two rectangles.
 ```
-Hello
+Input:    rec1 = [[0,0],           rec2 = [[0,0],
+                  [0,5]                    [0,1]
+                  [5,5]                    [1,1]
+                  [5,0]].                  [1,0]].
+Output: True
 ```
 #### 3) Sock merchant problem
+There is a large pile of socks that must be paired by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 ```
-Hello
+Input: arr=[1,2,1,2,1,3,2]
+Output: 2
 ```
